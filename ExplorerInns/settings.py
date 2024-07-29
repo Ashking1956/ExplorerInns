9faa8720f8b19ac12af699630e9e60970f054f95
[Ashking1956] <[ashishnirmal.cse21@aii.ac.in]>
2024-07-29 19:02:23 +0530

    Updated Database with more Indian Data!

--- ExplorerInns/settings.py
+++ ExplorerInns/settings.py
@@ -81,11 +81,8 @@ WSGI_APPLICATION = 'ExplorerInns.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'explorerinns',
-        'USER': 'postgres',
-        'PASSWORD': 'Maa2004@',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
